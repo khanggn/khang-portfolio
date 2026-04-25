ccc19b45ac5e4dc87d3e9b528f18d0f9dfc5be85
khanggn <khn042@ucsd.edu>
2026-04-24 20:41:53 -0700

    Fix: Convert API function to CommonJS for Vercel compatibility

--- api/visitors.js
+++ api/visitors.js
@@ -1,5 +1,5 @@
 // Vercel Serverless Function to fetch analytics data
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
@@ -60,4 +60,4 @@ export default async function handler(req, res) {
       details: error.message
     });
   }
-}
+};
